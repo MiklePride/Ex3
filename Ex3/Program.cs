e723b4919efe8692df1bc3efb46773e5b36b3989
Михаил Денисов <migelius.invesy@gmail.com>
2022-03-10 15:29:12 +0300

    Добавьте файлы проекта.

---
+++ Ex3/Program.cs
@@ -0,0 +1,11 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        int a = 10;
+        int b = 38;
+        int c = (31 - 5 * a) / b;
+        Console.WriteLine(c);
+        // Ответ: с = 0.5, но так, как тип переменной int, ответом и будет только целое число, по этому ответ 0.
+    }
+}
